b5e941654393bb8b287581d7fa83cbe84a73f1d3
hatuw <jiaxi_wu@ieee.org>
2018-12-31 11:39:53 +0800

    完善svmiter

--- include/svmiter.h
+++ include/svmiter.h
@@ -8,103 +8,146 @@
 
 using namespace std;
 
-class SVMIterator {
-    public:
-    static const int TERM_CODE = 0;
-    static const int G_CODE = 1;
-    static const int GP_CODE = 2;
-    static const int GM_CODE = 3;
-    static const int GMP_CODE = 4;
+class SVMIterator
+{
+public:
+  static const int TERM_CODE = 0;
+  static const int G_CODE = 1;
+  static const int GP_CODE = 2;
+  static const int GM_CODE = 3;
+  static const int GMP_CODE = 4;
 
-    explicit SVMIterator(MPCEnv& mpc, int pid) {
-        this -> mpc = &mpc;
-        this -> pid = pid;
+  explicit SVMIterator(MPCEnv &mpc, int pid)
+  {
+    this->mpc = &mpc;
+    this->pid = pid;
+  }
+
+  void Init(bool pheno_flag, bool missing_flag)
+  {
+    this->pheno_flag = pheno_flag;
+    this->missing_flag = missing_flag;
+    if (pid == 1)
+      mpc->SendInt(TransferMode(), 3);
+
+    cout << "Initialized SVMIterator" << endl;
+  }
+
+  void Terminate()
+  {
+    if (pid == 1)
+      mpc->SendInt(TERM_CODE, 3);
+  }
+
+  int TransferMode()
+  {
+    if (missing_flag)
+    {
+      if (pheno_flag)
+      {
+        return GMP_CODE;
+      }
+      else
+      {
+        return GM_CODE;
+      }
     }
-
-    void Init(bool pheno_flag, bool missing_flag) {
-        cout << "Initialized SVMIterator" << endl;
+    else
+    {
+      if (pheno_flag)
+      {
+        return GP_CODE;
+      }
+      else
+      {
+        return G_CODE;
+      }
     }
+  }
 
-    void Terminate() {
-        if (pid == 1) {
-            mpc -> SendInt(TERM_CODE, 3);
-        }
-    }
+  bool NotDone()
+  {
+    return num_left > 0;
+  }
 
-    int TransferMode() {
-        if (missing_flag) {
-            if (pheno_flag) {
-                return GMP_CODE;
-            } else {
-                return GM_CODE;
-            }
-        } else {
-          if (pheno_flag) {
-            return GP_CODE;
-          } else {
-            return G_CODE;
-          }
-        }
-    }
+  /* Return genotypes in dosage format (0, 1, or 2), assume no missing */
+  void GetNextGP(Vec<ZZ_p> &g, Vec<ZZ_p> &p)
+  {
+    assert(TransferMode() == GP_CODE);
+    Vec<ZZ_p> m;
+    Mat<ZZ_p> gmat;
+    GetNextAux(gmat, m, p);
+    g = gmat[0];
+  }
 
-    bool NotDone() {
-      return num_left > 0;
-    }
+  void GetNextG(Vec<ZZ_p> &g)
+  {
+    assert(TransferMode() == G_CODE);
+    Vec<ZZ_p> p;
+    Vec<ZZ_p> m;
+    Mat<ZZ_p> gmat;
+    GetNextAux(gmat, m, p);
+    g = gmat[0];
+  }
 
-    void GetNextGP(Vec<ZZ_p>& g, Vec<ZZ_p>& p) {
+  /* Return genotype probabilities with missingness information */
+  void GetNextGM(Mat<ZZ_p> &g, Vec<ZZ_p> &m)
+  {
+    assert(TransferMode() == GM_CODE);
+    Vec<ZZ_p> p;
+    GetNextAux(g, m, p);
+  }
 
-    }
-
-    void GetNextG(Vec<ZZ_p>& g) {
-
-    }
-
-    void GetNextGM(Mat<ZZ_p>& g, Vec<ZZ_p>& m) {
-
-    }
-
-    void GetNextGMP(Mat<ZZ_p>& g, Vec<ZZ_p>& m, Vec<ZZ_p>& p) {
-      assert(TransferMode() == GMP_CDOE);
-      GetNextAux(g, m, p);
-    }
+  void GetNextGMP(Mat<ZZ_p> &g, Vec<ZZ_p> &m, Vec<ZZ_p> &p)
+  {
+    assert(TransferMode() == GMP_CODE);
+    GetNextAux(g, m, p);
+  }
 
 private:
   MPCEnv *mpc;
   int pid;
   int num_left;
   int index;
-  bool pheno_flag;
-  bool missing_flat;
+  bool pheno_flag; // ???
+  bool missing_flag;
 
-  void GetNextAut(Mat<ZZ_p>& g, Vec<ZZ_p>& m, Vec<ZZ_p>& p) {
+  void GetNextAux(Mat<ZZ_p> &g, Vec<ZZ_p> &m, Vec<ZZ_p> &p)
+  {
     assert(NotDone());
 
-    if (missing_flat) {
-
-    } else {
-
+    if (missing_flag)
+    {
+    }
+    else
+    {
     }
 
-    if (pid == 2) {
-      if (pheno_flag) {
-
+    if (pid == 2)
+    {
+      if (pheno_flag)
+      {
       }
-      if (missing_flag) {
-
-      } else {
-
+      if (missing_flag)
+      {
       }
-    } else if (pid == 1) {
-      mpc -> SwitchSeed(3);
-      if (pheno_flag) {
-
+      else
+      {
       }
-      if (missing_flat) {
-
-      } else {
-
+    }
+    else if (pid == 1)
+    {
+      mpc->SwitchSeed(3);
+      if (pheno_flag)
+      {
       }
-      mpc -> RestoreSeed();
+      if (missing_flag)
+      {
+      }
+      else
+      {
+      }
+      mpc->RestoreSeed();
     }
 
     index++;
--- src/DataSharingClient.cpp
+++ src/DataSharingClient.cpp
@@ -1,4 +1,5 @@
 #include "mpc.h"
+#include "protocol.h"
 #include "util.h"
 #include "NTL/ZZ_p.h"
 #include "svmiter.h"
@@ -8,21 +9,30 @@
 using namespace NTL;
 using namespace std;
 
-int main(int argc, char** argv) {
-  if (argc < 3) {
+bool send_stream(string data_dir, MPCEnv &mpc)
+{
+  return true;
+}
+
+int main(int argc, char **argv)
+{
+  if (argc < 3)
+  {
     cout << "Usage: DataSharingClient party_id param_file [data_fir (for P3/SP)]" << endl;
   }
 
   /* Load party id */
   string pid_str(argv[1]);
   int pid;
-  if (!Param::Convert(pid_str, pid, "party_id") || pid < 0 || pid > 3) {
+  if (!Param::Convert(pid_str, pid, "party_id") || pid < 0 || pid > 3)
+  {
     cout << "Error: party_id should be 0, 1, 2, or 3" << endl;
     return 1;
   }
 
   /* Load values in parameter file. */
-  if (!Param::ParseFile(argv[2])) {
+  if (!Param::ParseFile(argv[2]))
+  {
     cout << "Could not finish parsing parameter file" << endl;
     return 1;
   }
@@ -30,14 +40,17 @@ int main(int argc, char** argv) {
   /* Load data directory name. */
   string data_dir;
   // pid == 3 --> P3/SP
-  if (pid == 3) {
-    if (argc < 4) {
+  if (pid == 3)
+  {
+    if (argc < 4)
+    {
       cout << "Error: for P3/SP, data directory should be probided as the last argument" << endl;
       return 1;
     }
     data_dir = argv[3];
     // ex:  bin/DataSharingClient 3 ../par/test.par.3.txt ../test_data/
-    if (data_dir[data_dir.size() - 1] != '/') {
+    if (data_dir[data_dir.size() - 1] != '/')
+    {
       data_dir += "/";
     }
 
@@ -45,7 +58,7 @@ int main(int argc, char** argv) {
   }
 
   /* Initialize MPC enviroment */
-  vector< pair<int, int> > pairs;
+  vector<pair<int, int>> pairs;
   pairs.push_back(make_pair(0, 1));
   pairs.push_back(make_pair(0, 2));
   pairs.push_back(make_pair(1, 2));
@@ -53,24 +66,57 @@ int main(int argc, char** argv) {
   pairs.push_back(make_pair(2, 3));
 
   MPCEnv mpc;
-  if (!mpc.Initialize(pid ,pairs)) {
+  if (!mpc.Initialize(pid, pairs))
+  {
     cout << "MPC environment initialization failed" << endl;
     return 1;
   }
 
   /* Mask the data and save to file. */
   bool success;
-  if (pid < 3) {
-    // success = 
-  } else {
+  if (pid < 3)
+  {
+    success = svm_data_sharing_protocol(mpc, pid);
+  }
+  else
+  {
     // pid == 3 --> P3/SP
-    int signal = mpcReceiveInt(1);
+    int signal = mpc.ReceiveInt(1);
 
-    // while (signal != SVMIterator::TERM_CODE) {
-    //   success = send_stream(data_dir, mpc);
-    //   if (!success) break;
+    while (signal != SVMIterator::TERM_CODE)
+    {
+      success = send_stream(data_dir, mpc);
+      if (!success)
+        break;
 
-    //   signal = mpc.ReceiveInt(1);
-    // }
+      signal = mpc.ReceiveInt(1);
+    }
+
+    cout << "Done with streaming data" << endl;
+    success = true;
+  }
+
+  // Thiss is here just to keep P0 online until the end for data transfer
+  // In pratice, P0 would send data in advance before each phase and go oofline
+  if (pid == 0)
+  {
+    mpc.ReceiveBool(2);
+  }
+  else if (pid == 2)
+  {
+    mpc.SendBool(true, 0);
+  }
+
+  mpc.CleanUp();
+
+  if (success)
+  {
+    cout << "Protocol successfully completed" << endl;
+    return 0;
+  }
+  else
+  {
+    cout << "Protocol abnormally terminated" << endl;
+    return 1;
   }
 }
