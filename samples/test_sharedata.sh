272193084955b5d917f72cd2144aec6f2919f7f3
hatuw <jiaxi_wu@ieee.org>
2019-01-05 15:29:59 +0800

    add sharedata testing script

---
+++ samples/test_sharedata.sh
@@ -0,0 +1,6 @@
+# Mask and share data.
+../bin/ShareData 0 ../par/test.par.0.txt &
+../bin/ShareData 1 ../par/test.par.1.txt &
+../bin/ShareData 2 ../par/test.par.2.txt &
+../bin/ShareData 3 ../par/test.par.3.txt ../res/data/ &
+wait
